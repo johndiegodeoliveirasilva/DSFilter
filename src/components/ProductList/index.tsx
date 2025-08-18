1a1e2776cc132c4e450625936ac5565b80c6086b
John Diego <johnoliveirasilva9@gmail.com>
2025-08-18 11:18:30 -0300

    create list products and show

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,34 +1,15 @@
-export default function ProductList() {
+import type { ProductDTO } from "../../assets/models/product";
+
+type Props = {
+    product: ProductDTO;
+}
+
+export default function ProductList({ product}: Props) {
   return (
     <>
-      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50">
-        <div className="text-gray-700 text-sm mb-1">PC Gamer Pro</div>
-        <div className="text-green-600 font-semibold text-lg">R$ 1200.00</div>
-      </div>
-
-      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50">
-        <div className="text-gray-700 text-sm mb-1">PC Gamer Pro</div>
-        <div className="text-green-600 font-semibold text-lg">R$ 1200.00</div>
-      </div>
-
-      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50">
-        <div className="text-gray-700 text-sm mb-1">PC Gamer Pro</div>
-        <div className="text-green-600 font-semibold text-lg">R$ 1200.00</div>
-      </div>
-
-      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50">
-        <div className="text-gray-700 text-sm mb-1">PC Gamer Pro</div>
-        <div className="text-green-600 font-semibold text-lg">R$ 1200.00</div>
-      </div>
-
-      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50">
-        <div className="text-gray-700 text-sm mb-1">PC Gamer Pro</div>
-        <div className="text-green-600 font-semibold text-lg">R$ 1200.00</div>
-      </div>
-
-      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50">
-        <div className="text-gray-700 text-sm mb-1">PC Gamer Pro</div>
-        <div className="text-green-600 font-semibold text-lg">R$ 1200.00</div>
+      <div className="border-2 border-green-400 rounded-md p-4 bg-green-50" key={product.id}>
+        <div className="text-gray-700 text-sm mb-1">{product.name}</div>
+        <div className="text-green-600 font-semibold text-lg">R$ {product.price}</div>
       </div>
     </>
   );
--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,8 +1,19 @@
+import { use, useEffect, useState } from "react";
 import Filter from "../../components/Filter";
 import Header from "../../components/Header";
 import ProductList from "../../components/ProductList";
+import * as productService from "../../services/product-services";
+import type { ProductDTO } from "../../assets/models/product";
 
 export default function Dashboard() {
+
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+
+  useEffect(() => {
+    setProducts(productService.findByPrice(0, 10000))
+  })
+  
   return (
     <>
       <body className="bg-gray-300 min-h-screen">
@@ -11,7 +22,9 @@ export default function Dashboard() {
           <Filter />
           <div className="bg-white rounded-lg shadow-sm p-6">
             <div className="space-y-4">
-              <ProductList />
+              { products.map((product) => (
+              <ProductList product={product} />
+              ))}
             </div>
           </div>
         </div>
