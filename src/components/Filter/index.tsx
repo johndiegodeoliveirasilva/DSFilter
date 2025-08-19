98f5a4cb90fb40dc142f6feded01d2cb1ce29d8b
John Diego <johnoliveirasilva9@gmail.com>
2025-08-18 21:45:47 -0300

    create event filter

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,27 +1,76 @@
-export default function Filter() {
+import { useState } from "react";
+
+type Props = {
+  onFilter: Function;
+};
+
+type FormDate = {
+  minPrice?: number;
+  maxPrice?: number;
+};
+
+export default function Filter({ onFilter }: Props) {
+  const [formDate, setFormDate] = useState<FormDate>({
+    minPrice: undefined,
+    maxPrice: undefined,
+  });
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+
+    const queryParams = {
+      minPrice: formDate.minPrice || 0,
+      maxPrice: formDate.maxPrice || Number.MAX_VALUE,
+    };
+
+    onFilter(queryParams);
+  }
+
+  function handleInpultChange(event: any) {
+    const { value, name } = event.target;
+    setFormDate({
+      ...formDate,
+      [name]: value,
+    });
+  }
+
   return (
-    <div className="bg-white rounded-lg shadow-sm p-6 mb-6">
-      <div className="max-w-sm space-y-4">
-        <div>
-          <input
-            type="text"
-            placeholder="Preço mínimo"
-            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-          />
-        </div>
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white rounded-lg shadow-sm p-6 mb-6"
+      >
+        <div className="max-w-sm space-y-4">
+          <div>
+            <input
+              type="text"
+              placeholder="Preço mínimo"
+              onChange={handleInpultChange}
+              name="minPrice"
+              value={formDate.minPrice || ""}
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+            />
+          </div>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Preço máximo"
-            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-          />
-        </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Preço máximo"
+              onChange={handleInpultChange}
+              value={formDate.maxPrice || ""}
+              name="maxPrice"
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+            />
+          </div>
 
-        <button className="w-full bg-green-500 text-white py-3 px-4 rounded-md hover:bg-green-600 transition-colors font-medium">
-          Filtrar
-        </button>
-      </div>
-    </div>
+          <button
+            type="submit"
+            className="w-full bg-green-500 text-white py-3 px-4 rounded-md hover:bg-green-600 transition-colors font-medium"
+          >
+            Filtrar
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -6,38 +6,45 @@ import * as productService from "../../services/product-services";
 import type { ProductDTO } from "../../assets/models/product";
 
 type QueryParams = {
-  minPrice?: number;
-  maxPrice?: number;
-}
+  minPrice: number;
+  maxPrice: number;
+};
 
 export default function Dashboard() {
-
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   const [queryParams, setQueryParams] = useState<QueryParams>({
     minPrice: 0,
-    maxPrice: Number.MAX_VALUE
-  })
+    maxPrice: Number.MAX_VALUE,
+  });
 
+  function handleFilter(queryParams: string) {
+    setProducts([]);
+    setQueryParams({
+      ...queryParams,
+      minPrice: queryParams.minPrice,
+      maxPrice: queryParams.maxPrice,
+    });
+  }
   useEffect(() => {
-    setProducts(productService.findByPrice(0, 10000))
-  })
-  
+    setProducts(productService.findByPrice(queryParams.minPrice, queryParams.maxPrice));
+  }, [queryParams]);
+
   return (
     <>
-      <body className="bg-gray-300 min-h-screen">
+      <div className="bg-gray-300 min-h-screen">
         <Header />
         <div className="max-w-6xl mx-auto p-6">
-          <Filter />
+          <Filter onFilter={handleFilter} />
           <div className="bg-white rounded-lg shadow-sm p-6">
             <div className="space-y-4">
-              { products.map((product) => (
-              <ProductList product={product} />
+              {products.map((product) => (
+                <ProductList key={product.id} product={product} />
               ))}
             </div>
           </div>
         </div>
-      </body>
+      </div>
     </>
   );
 }
